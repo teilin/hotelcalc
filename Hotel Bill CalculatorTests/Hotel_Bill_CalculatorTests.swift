d6065520b19eacd0660f8f0ef94e617343413317
Teis Lindemark <teis.lindemark@gmail.com>
2016-02-24 18:54:42 +0100

    Initial Commit

---
+++ Hotel Bill CalculatorTests/Hotel_Bill_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hotel_Bill_CalculatorTests.swift
+//  Hotel Bill CalculatorTests
+//
+//  Created by Teis Lindemark on 24.02.2016.
+//  Copyright © 2016 Teis Lindemark. All rights reserved.
+//
+
+import XCTest
+@testable import Hotel_Bill_Calculator
+
+class Hotel_Bill_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
